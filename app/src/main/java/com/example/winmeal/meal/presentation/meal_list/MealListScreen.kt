1ffeacad2285459f3572b0b61e5a730b6ebe3a91
Uzair Ansari <124576222+UzairAnsari054@users.noreply.github.com>
2025-12-19 17:14:45 +0530

    Revert "revert"
    This reverts commit 8b9156240ba6792aed5581d7b3ea6411e499dbf8.

--- app/src/main/java/com/example/winmeal/meal/presentation/meal_list/MealListScreen.kt
+++ app/src/main/java/com/example/winmeal/meal/presentation/meal_list/MealListScreen.kt
@@ -1,21 +1,43 @@
 package com.example.winmeal.meal.presentation.meal_list
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.winmeal.R
 import com.example.winmeal.core.presentation.theme.DarkBlue
+import com.example.winmeal.core.presentation.theme.DesertWhite
+import com.example.winmeal.core.presentation.theme.SandYellow
 import com.example.winmeal.meal.domain.Meal
+import com.example.winmeal.meal.presentation.meal_list.components.MealList
 import com.example.winmeal.meal.presentation.meal_list.components.MealSearchBar
 import org.koin.androidx.compose.koinViewModel
 
@@ -44,6 +66,22 @@ private fun MealListScreen(
     onAction: (MealListAction) -> Unit
 ) {
     val keyboardController = LocalSoftwareKeyboardController.current
+    val pagerState = rememberPagerState { 2 }
+
+    val searchResultsListState = rememberLazyListState()
+    val favoriteMealsListState = rememberLazyListState()
+
+    LaunchedEffect(state.searchResult) {
+        searchResultsListState.animateScrollToItem(0)
+    }
+
+    LaunchedEffect(state.selectedTabIndex) {
+        pagerState.animateScrollToPage(state.selectedTabIndex)
+    }
+
+    LaunchedEffect(pagerState.currentPage) {
+        onAction(MealListAction.OnTabChanged(pagerState.currentPage))
+    }
 
     Column(
         modifier = Modifier
@@ -64,13 +102,157 @@ private fun MealListScreen(
                 .fillMaxWidth()
                 .padding(16.dp)
         )
+
+        Surface(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth(),
+            color = DesertWhite,
+            shape = RoundedCornerShape(
+                topStart = 32.dp,
+                topEnd = 32.dp
+            )
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                TabRow(
+                    selectedTabIndex = state.selectedTabIndex,
+                    modifier = Modifier
+                        .padding(vertical = 12.dp)
+                        .fillMaxWidth(),
+                    containerColor = DesertWhite,
+                    indicator = { tabPositions ->
+                        TabRowDefaults.SecondaryIndicator(
+                            color = SandYellow,
+                            modifier = Modifier.tabIndicatorOffset(tabPositions[state.selectedTabIndex])
+                        )
+                    }
+                ) {
+                    Tab(
+                        selected = state.selectedTabIndex == 0,
+                        onClick = {
+                            onAction(MealListAction.OnTabChanged(0))
+                        },
+                        modifier = Modifier.weight(1f),
+                        selectedContentColor = SandYellow,
+                        unselectedContentColor = Color.Black.copy(alpha = 0.5f),
+                    ) {
+                        Text(
+                            text = stringResource(R.string.search_results),
+                            modifier = Modifier.padding(vertical = 12.dp)
+                        )
+                    }
+
+                    Tab(
+                        selected = state.selectedTabIndex == 1,
+                        onClick = {
+                            onAction(MealListAction.OnTabChanged(1))
+                        },
+                        modifier = Modifier.weight(1f),
+                        selectedContentColor = SandYellow,
+                        unselectedContentColor = Color.Black.copy(alpha = 0.5f),
+                    ) {
+                        Text(
+                            text = stringResource(R.string.favorites),
+                            modifier = Modifier.padding(vertical = 12.dp)
+                        )
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                HorizontalPager(
+                    state = pagerState,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                ) { pageIndex ->
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        when (pageIndex) {
+                            0 -> {
+                                if (state.isLoading) {
+                                    CircularProgressIndicator()
+                                } else {
+                                    when {
+                                        state.errorMessage != null -> {
+                                            Text(
+                                                text = state.errorMessage.asString(),
+                                                textAlign = TextAlign.Center,
+                                                style = MaterialTheme.typography.headlineSmall,
+                                                color = MaterialTheme.colorScheme.error
+                                            )
+                                        }
+
+                                        state.searchResult.isEmpty() -> {
+                                            Text(
+                                                text = stringResource(R.string.no_search_results),
+                                                textAlign = TextAlign.Center,
+                                                style = MaterialTheme.typography.headlineSmall,
+                                                color = MaterialTheme.colorScheme.error
+                                            )
+                                        }
+
+                                        else -> {
+                                            MealList(
+                                                meals = state.searchResult,
+                                                onMealClick = {
+                                                    onAction(MealListAction.OnMealClick(it))
+                                                },
+                                                modifier = Modifier.fillMaxSize(),
+                                                scrollState = searchResultsListState
+                                            )
+                                        }
+                                    }
+                                }
+                            }
+
+                            1 -> {
+                                if (state.favoriteMeals.isEmpty()) {
+                                    Text(
+                                        text = stringResource(R.string.no_favorite_meals),
+                                        textAlign = TextAlign.Center,
+                                        style = MaterialTheme.typography.headlineSmall,
+                                    )
+                                } else {
+                                    MealList(
+                                        meals = state.searchResult,
+                                        onMealClick = {
+                                            onAction(MealListAction.OnMealClick(it))
+                                        },
+                                        modifier = Modifier.fillMaxSize(),
+                                        scrollState = favoriteMealsListState
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
+}
+
+private val meals = (1..100).map {
+    Meal(
+        id = it.toString(),
+        name = "Beef Stew $it",
+        imageUrl = "https://www.test.com",
+        category = "Beef",
+        origin = "Indian"
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 private fun MealListScreenPreview() {
-    val state = MealListState()
+    val state = MealListState(
+        searchResult = meals,
+    )
+
     MealListScreen(
         state = state,
         onAction = {})
--- app/src/main/java/com/example/winmeal/meal/presentation/meal_list/MealListState.kt
+++ app/src/main/java/com/example/winmeal/meal/presentation/meal_list/MealListState.kt
@@ -4,9 +4,20 @@ import com.example.winmeal.core.presentation.UiText
 import com.example.winmeal.meal.domain.Meal
 
 data class MealListState(
-    val isLoading: Boolean = true,
+    val isLoading: Boolean = false,
     val errorMessage: UiText? = null,
     val searchQuery: String = "Beef",
-    val searchResult: List<Meal> = emptyList(),
+    val searchResult: List<Meal>  =meals,
+    val favoriteMeals: List<Meal> = emptyList(),
     val selectedTabIndex: Int = 0
 )
+
+private val meals = (1..100).map {
+    Meal(
+        id = it.toString(),
+        name = "Beef Stew $it",
+        imageUrl = "https://www.test.com",
+        category = "Beef",
+        origin = "Indian"
+    )
+}
